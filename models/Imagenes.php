9752ca15347be1be4a2e67e0fa174f19f9f3176b
gerard <gerardobritez.gb@gmail.com>
2019-06-28 13:14:33 -0300

    funcionando completo modulo materiales, categorias,productos,listo para nueva rama

--- models/Imagenes.php
+++ models/Imagenes.php
@@ -11,19 +11,9 @@
             $sql= "INSERT INTO imagenes (descripcion, producto_id) VALUES ('$descripcion','$producto_id')";
             return ejectuarConsulta($sql);
         }
-        Public function editar($idimagen,$descripcion)
-        {
-            $sql = "UPDATE imagenes SET descripcion ='$descripcion' WHERE id_imagen = '$idimagen' ";
-            return ejectuarConsulta($sql);
-        }
         Public  function mostrar($producto_id)
         {
             $sql = "SELECT * FROM imagenes WHERE producto_id='$producto_id' ";
-            return ejectuarConsulta($sql);
-        }
-        Public function listar()
-        {
-            $sql= "SELECT * FROM imagenes";
             return ejectuarConsulta($sql);
         }
         public function eliminar($idimagen){
